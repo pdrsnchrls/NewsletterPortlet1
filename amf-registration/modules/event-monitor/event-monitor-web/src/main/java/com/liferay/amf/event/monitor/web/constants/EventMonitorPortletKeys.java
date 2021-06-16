4f25906ad1b92e473da3dc5ff470facb40f2116d
pdrsnchrls <charles.pederson@liferay.com>
2021-06-16 13:18:25 -0700

    REG-2 Add event-monitor-web

---
+++ amf-registration/modules/event-monitor/event-monitor-web/src/main/java/com/liferay/amf/event/monitor/web/constants/EventMonitorPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.event.monitor.web.constants;
+
+/**
+ * @author charles
+ */
+public class EventMonitorPortletKeys {
+
+	public static final String EVENTMONITOR =
+		"com_liferay_amf_event_monitor_web_EventMonitorPortlet";
+
+}
