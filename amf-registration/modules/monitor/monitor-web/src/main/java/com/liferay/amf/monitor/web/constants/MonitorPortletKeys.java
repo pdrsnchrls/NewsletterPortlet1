10f8eef7f13d013d24da8393876e460834518555
pdrsnchrls <charles.pederson@liferay.com>
2021-06-17 10:53:50 -0700

    REG-2 Create monitor-web portlet

---
+++ amf-registration/modules/monitor/monitor-web/src/main/java/com/liferay/amf/monitor/web/constants/MonitorPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.monitor.web.constants;
+
+/**
+ * @author charles
+ */
+public class MonitorPortletKeys {
+
+	public static final String MONITOR =
+		"com_liferay_amf_monitor_web_MonitorPortlet";
+
+}
---
+++ amf-registration/modules/monitor/monitor-web/src/main/java/com/liferay/amf/monitor/web/portlet/MonitorPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.amf.monitor.web.portlet;
+
+import com.liferay.amf.monitor.web.constants.MonitorPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author charles
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=Monitor",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + MonitorPortletKeys.MONITOR,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class MonitorPortlet extends MVCPortlet {
+}
