f16c5ea4b4493ace5e24f475f680eab7a2c15161
pdrsnchrls <charles.pederson@liferay.com>
2021-06-22 10:07:14 -0700

    REG-3 Create search module portlet and initialize under category.amf

---
+++ amf-registration/modules/amf-search/amf-search-web/src/main/java/com/liferay/amf/search/web/constants/SearchPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.search.web.constants;
+
+/**
+ * @author charles
+ */
+public class SearchPortletKeys {
+
+	public static final String SEARCH =
+		"com_liferay_amf_search_web_portlet_SearchPortlet";
+
+}
