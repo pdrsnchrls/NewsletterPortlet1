25fb2276e9524133308384689cc4e0a070281276
pdrsnchrls <charles.pederson@liferay.com>
2021-06-22 10:39:09 -0700

    REG-3 add validation function and ran serviceBuilder

---
+++ amf-registration/modules/amf-search/amf-search-service/src/main/java/com/liferay/amf/search/service/util/validator/SearchValidator.java
@@ -0,0 +1,42 @@
+package com.liferay.amf.search.service.util.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchValidator {
+
+	@Override
+	public void validate(String zip)
+	{
+		List<String> errors = new ArrayList<>();
+		
+		if (!isZipValid(zip, errors)) {
+			System.out.println("Invalid Zip!");
+		}
+	}
+	
+	//ZIP Code
+	public boolean isZipValid(
+			final String zip, final List<String> errors) {
+		boolean result = true;	
+		if (zip.isEmpty()) {
+			result = false;
+			errors.add("zipEmpty");
+		}
+		if (zip.length() != 5 & !zip.isEmpty()) {
+			result = false;
+			errors.add("zipLengthError");
+		}
+		if (!zip.isEmpty()) {
+			try {
+				int num = Integer.parseInt(zip);
+				
+			} catch (NumberFormatException e) {
+				result = false;
+				errors.add("zipNonNumeric");
+			}
+		}
+		return result;
+	}
+	
+}
