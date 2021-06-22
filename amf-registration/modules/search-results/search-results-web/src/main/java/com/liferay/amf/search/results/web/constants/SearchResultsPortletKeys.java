eb9e94454466551a20e0eadecc09e3a2b9f2622e
pdrsnchrls <charles.pederson@liferay.com>
2021-06-22 15:18:37 -0700

    REG-3 Add web side of search results portlet

---
+++ amf-registration/modules/search-results/search-results-web/src/main/java/com/liferay/amf/search/results/web/constants/SearchResultsPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.search.results.web.constants;
+
+/**
+ * @author charles
+ */
+public class SearchResultsPortletKeys {
+
+	public static final String SEARCHRESULTS =
+		"com_liferay_amf_search_results_web_SearchResultsPortlet";
+
+}
