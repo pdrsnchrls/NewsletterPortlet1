f1516709224d03b53387b0338f1543dc90d067fc
pdrsnchrls <charles.pederson@liferay.com>
2021-06-28 15:58:25 -0700

    NEW-1 Initial commit. Create newsletter-web. Edit service.xml

---
+++ amf-registration/modules/newsletter/newsletter-web/src/main/java/com/liferay/amf/newsletter/web/constants/NewsletterPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.newsletter.web.constants;
+
+/**
+ * @author charles
+ */
+public class NewsletterPortletKeys {
+
+	public static final String NEWSLETTER =
+		"com_liferay_amf_newsletter_web__portlet_NewsletterPortlet";
+
+}
---
+++ amf-registration/modules/newsletter/newsletter-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="newsletter.caption"/></b>
+</p>
