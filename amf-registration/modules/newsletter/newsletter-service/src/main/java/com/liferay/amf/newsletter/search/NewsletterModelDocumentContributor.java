4603e037f1d7ca1a4ffce5f9d20ca51b374da523
pdrsnchrls <charles.pederson@liferay.com>
2021-08-03 15:59:13 -0700

    NEWS-2 Begin adding indexer for Newsletter

---
+++ amf-registration/modules/newsletter/newsletter-service/src/main/java/com/liferay/amf/newsletter/search/NewsletterModelDocumentContributor.java
@@ -0,0 +1,71 @@
+package com.liferay.amf.newsletter.search;
+
+import com.liferay.amf.newsletter.model.Issue;
+import com.liferay.amf.newsletter.model.Newsletter;
+import com.liferay.amf.newsletter.service.IssueLocalService;
+import com.liferay.amf.newsletter.service.NewsletterLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.Locale;
+
+@Component(
+        immediate = true,
+        property = "indexer.class.name=com.liferay.amf.newsletter.model.Newsletter",
+        service = ModelDocumentContributor.class
+)
+public class NewsletterModelDocumentContributor implements ModelDocumentContributor<Newsletter> {
+    @Override
+    public void contribute(Document document, Newsletter newsletter) {
+        try {
+            Locale defaultLocale = LocaleUtil.getDefault();
+
+            String localizedTitle = LocalizationUtil.getLocalizedName(
+                    Field.TITLE, defaultLocale.toString());
+            String localizedContent = LocalizationUtil.getLocalizedName(
+                    Field.CONTENT, defaultLocale.toString());
+
+            document.addText(localizedTitle, newsletter.getTitle());
+            document.addText(localizedContent, newsletter.getContent());
+
+            document.addText("newsletterAuthor", newsletter.getAuthor());
+            document.addNumber("newsletterIssueNumber", newsletter.getIssueNumber());
+            document.addNumber("newsletterId", newsletter.getNewsletterId());
+
+            long issueNumber = newsletter.getIssueNumber();
+
+            Issue issue = _issueLocalService.getIssueByIssueNumber(issueNumber);
+
+            String issueTitle = issue.getTitle();
+
+            String localizedGbName = LocalizationUtil.getLocalizedName(
+                    Field.NAME, defaultLocale.toString());
+
+            document.addText(localizedGbName, issueTitle);
+        } catch (PortalException pe) {
+            if (_log.isWarnEnabled()) {
+                _log.warn("Unable to index newsletter " + newsletter.getNewsletterId(), pe);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final Log _log = LogFactoryUtil.getLog(
+            NewsletterModelDocumentContributor.class);
+
+    @Reference
+    private IssueLocalService _issueLocalService;
+
+    @Reference
+    private NewsletterLocalService _newsletterLocalService;
+}
---
+++ amf-registration/modules/newsletter/newsletter-service/src/main/java/com/liferay/amf/newsletter/search/NewsletterSearchRegistrar.java
@@ -0,0 +1,56 @@
+package com.liferay.amf.newsletter.search;
+
+import com.liferay.amf.newsletter.model.Newsletter;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelIndexerWriterContributor;
+import com.liferay.portal.search.spi.model.registrar.ModelSearchRegistrarHelper;
+import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(immediate = true)
+public class NewsletterSearchRegistrar {
+
+    @Activate
+    protected void activate(BundleContext bundleContext) {
+
+        _serviceRegistration = modelSearchRegistrarHelper.register(
+                Newsletter.class, bundleContext, modelSearchDefinition -> {
+                    modelSearchDefinition.setDefaultSelectedFieldNames(
+                            Field.COMPANY_ID, Field.ENTRY_CLASS_NAME,
+                            Field.ENTRY_CLASS_PK, Field.UID,
+                            Field.SCOPE_GROUP_ID, Field.GROUP_ID);
+
+                    modelSearchDefinition.setDefaultSelectedLocalizedFieldNames(
+                            Field.TITLE, Field.CONTENT);
+
+                    modelSearchDefinition.setModelIndexWriteContributor(
+                            modelIndexWriterContributor);
+                    modelSearchDefinition.setModelSummaryContributor(
+                            modelSummaryContributor);
+                    modelSearchDefinition.setSelectAllLocales(true);
+
+                });
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        _serviceRegistration.unregister();
+    }
+
+    @Reference(target = "(indexer.class.name=com.liferay.amf.newsletter.model.Newsletter)")
+    protected ModelIndexerWriterContributor<Newsletter> modelIndexWriterContributor;
+
+    @Reference
+    protected ModelSearchRegistrarHelper modelSearchRegistrarHelper;
+
+    @Reference(target = "(indexer.class.name=com.liferay.amf.newsletter.model.Newsletter)")
+    protected ModelSummaryContributor modelSummaryContributor;
+
+    private ServiceRegistration<?> _serviceRegistration;
+
+}
